c1e8ff9f0333fb6c9c59f28b030493660448ac4f
Andrey Andriynov <a.andriynov@pa-resultant.ru>
2024-02-05 15:55:33 +0300

    add link

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,10 +1,23 @@
+import Link from 'next/link';
 import styled from 'styled-components';
 
-export const Footer = styled.footer`
+const Container = styled.footer`
   height: var(--footer-height);
   width: 100dvw;
 
   display: flex;
   background-color: #414141;
   box-shadow: 0px -4px 5px 3px #656565;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
+
+export const Footer = () => {
+  return (
+    <Container>
+      <Link href={'https://rawg.io/'}> Данные взяты из https://rawg.io/</Link>
+    </Container>
+  );
+};
